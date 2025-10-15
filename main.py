2c52c25f6780106d8aa1764b2eb1925aaeb0673f
hanz <hanz@educ.college-montreal.qc.ca>
2025-10-15 14:53:11 -0400

    tp3 v2.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# auteur: Zhuo.H, classe 407
-# Ce programme est de faire un combat de monstre
+"""
+auteur: Zhuo.H, classe 407
+Ce programme est de faire un combat de monstre
 
+"""
 
 import random
-import time
 
 print("Vous êtes coincé dans ce donjon où le seul façon de sortir c'est de battre niveau 3!")
 print("Après chaque niveau, vous receverais d'esperience où vous pouvez l'utiliser pour améliorer vos abilité.")
@@ -37,9 +38,7 @@ while True:
         alive = True
         while alive:
             print(f" Le boss a {m_health} hp")
-            # time.sleep(0.5)
             print(f"vous avez: {health}hp")
-            # time.sleep(0.5)
             if turn == 1:
                 decision = input(f"Voulez vous attaquer(1), regénerer(2), tutoriel(3) ou quitter(4)?")
                 try:
@@ -48,29 +47,22 @@ while True:
                     print("réponse non accepter! veuiller recomencer")
                     continue
                 if decision == 1:
-                    # time.sleep(0.5)
                     print("vous attaquer!")
-                    # time.sleep(0.5)
                     result = dice(6)
                     m_result = dice(6)
                     print(f"Vous avez eu un:{result} et le monstre a un:{m_result}")
                     difference = result - m_result
-                    # time.sleep(0.5)
                     if difference >= 4:
-                        # time.sleep(0.5)
                         print("Dégas critique!")
                         m_health = m_health - 2 * damage
                     elif 1 <= difference <= 3:
-                        # time.sleep(0.5)
                         print(f"attaque reussite! vous faite: {damage} dégas.")
                         m_health = m_health - damage
                     else:
-                        # time.sleep(0.5)
                         print("Attaque non réussit!")
                 elif decision == 2:
                     regenerate = dice(4)
                     health = health + regenerate
-                    # time.sleep(0.5)
                     print(f"vous ajouter: {regenerate} hp!")
                 elif decision == 3:
                     print("Le but du jeu c'est de battre le boss au niveau 4.")
@@ -85,7 +77,6 @@ while True:
 
                 else:
                     print("Écrivez 1, 2,3 ou 4!")
-                    # time.sleep(0.5)
                     continue
 
             # monster AI section
@@ -97,25 +88,19 @@ while True:
                 choice = dice(3)
                 if choice == 1 or choice == 2:
                     print("le boss vous attaque!")
-                    # time.sleep(0.5)
                     print("vous devez avoir un resultat plus ou égal qu'au monstre pour que tu ne subis pas de dégas!")
-                    # time.sleep(0.5)
                     m_score = dice(6)
                     score = dice(6)
                     difference = m_score - score
                     print(f"le monstre a un {m_score} et vous avez eu un {score}")
-                    # time.sleep(0.5)
                     if difference >= 4:
                         print("le boss a fait une attaque critique!")
-                        # time.sleep(0.5)
                         print(f"le boss a fait {m_damage * 2} de dégas!")
                         health = health - m_damage * 2
                     elif 1 <= difference <= 3:
-                        # time.sleep(0.5)
                         print(f"le boss a fait {m_damage} de dégas!")
                         health = health - m_damage
                     else:
-                        # time.sleep(0.5)
                         print("le monstre a rater son coûp!")
                 else:
                     m_regenerate = dice(6)
@@ -134,11 +119,9 @@ while True:
                     damage = 5
                     level = 1
                     turn = 1
-                    # time.sleep(0.5)
                     print("restarting...")
                     break
                 elif restart == "n":
-                    # time.sleep(0.5)
                     print("Merci de jouer!")
                     exit()
             elif health <= 0:
@@ -152,18 +135,13 @@ while True:
                     damage = 5
                     level = 1
                     turn = 1
-                    # time.sleep(0.5)
                     print("restarting...")
                 elif restart == "n":
-                    # time.sleep(0.5)
                     print("Merci de jouer!")
                     exit()
 
     print(f"niveau {m_level}. Le monstre a {m_health} hp")
-    # time.sleep(0.5)
     print(f"vous avez: {health}hp")
-    # time.sleep(0.5)
-
     # user code
 
     if turn == 1:
@@ -174,29 +152,22 @@ while True:
             print("réponse non accepter! veuiller recomencer")
             continue
         if decision == 1:
-            # time.sleep(0.5)
             print("vous attaquer!")
-            # time.sleep(0.5)
             result = dice(6)
             m_result = dice(6)
             print(f"Vous avez eu un:{result} et le monstre a un:{m_result}")
             difference = result - m_result
-            # time.sleep(0.5)
             if difference >= 4:
-                # time.sleep(0.5)
                 print("Dégas critique!")
                 m_health = m_health - 2 * damage
             elif 1 <= difference <= 3:
-                # time.sleep(0.5)
                 print(f"attaque reussite! vous faite: {damage} dégas.")
                 m_health = m_health - damage
             else:
-                # time.sleep(0.5)
                 print("Attaque non réussit!")
         elif decision == 2:
             regenerate = dice(4)
             health += regenerate
-            # time.sleep(0.5)
             print(f"vous ajouter: {regenerate} hp!")
         elif decision == 3:
             m_level = m_level + 1
@@ -207,7 +178,7 @@ while True:
             print("Le but du jeu c'est de battre le boss au niveau 4.")
             print("Vous améliorez votre experience par tuer des montre.")
             print("Dans les combats, vous pouvez attaquer ou regenèrer")
-            print(f"vous devez rouler un dée avec le monstre et si votre nombre est plus grande, vous pouvez faire du dégas.")
+            print(f"vous devez rouler un dée avec le monstre et si votre nombre est plus grande, vous ferez du dégas.")
             print("Vous ne pouvez pas contourner le boss!")
             continue
         elif decision == 5:
@@ -215,36 +186,28 @@ while True:
             exit()
         else:
             print("Écrivez 1, 2, 3, 4 ou 5!")
-            # time.sleep(0.5)
             continue
 
     # monster AI section
 
     elif turn == -1:
-        # time.sleep(0.5)
         print("c'est au tour du monstre!")
         choice = dice(3)
         if choice == 1 or choice == 2:
             print("le monstre vous attaque!")
-            # time.sleep(0.5)
             print("vous devez avoir un resultat plus ou égal qu'au monstre pour que tu ne subis pas de dégas!")
-            # time.sleep(0.5)
             m_score = dice(6)
             score = dice(6)
             difference = m_score - score
             print(f"le monstre a un {m_score} et vous avez eu un {score}")
-            # time.sleep(0.5)
             if difference >= 4:
                 print("le monstre a fait une attaque critique!")
-                # time.sleep(0.5)
                 print(f"le monstre a fait {m_damage * 2} de dégas!")
                 health = health - m_damage * 2
             elif 1 <= difference <= 3:
-                # time.sleep(0.5)
                 print(f"le monstre a fait {m_damage} de dégas!")
                 health = health - m_damage
             else:
-                # time.sleep(0.5)
                 print("le monstre a rater son coûp!")
         else:
             m_regenerate = dice(4)
@@ -270,10 +233,8 @@ while True:
             damage = 5
             level = 1
             turn = 1
-            # time.sleep(0.5)
             print("restarting...")
         elif restart == "n":
-            # time.sleep(0.5)
             print("Merci de jouer!")
             exit()
     turn *= -1
